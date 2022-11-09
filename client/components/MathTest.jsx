d04bde699d3f96ab2c5f2037dbe9dc60ea53faaa
ashley cao <112398477+ashley-cao-0@users.noreply.github.com>
2022-11-10 12:13:44 +1300

    add a sum state

--- client/components/MathTest.jsx
+++ client/components/MathTest.jsx
@@ -4,14 +4,16 @@ function MathTest() {
   const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
   const [num1, setNum1] = useState(randomNum(10,99))
   const [num2, setNum2] = useState(randomNum(10,99))
+  const [sum, setSum] = useState(num1+num2)
   
   return (
     <div>
       <div>
         <h2>Math</h2>
         <div className=" bg-white w-1/3 mx-auto mt-16 p-4 rounded-md h-36 flex items-center justify-center">
-          <h2 className=" text-3xl">{num1} + {num2}</h2>
+          <h2 className=" text-4xl">{num1} + {num2}</h2>
         </div>
+        
       </div>
     </div>
   )
