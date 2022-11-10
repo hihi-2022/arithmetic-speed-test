4c2ae0bd82a9bf55a070e412d72def7dc714902b
ashley cao <112398477+ashley-cao-0@users.noreply.github.com>
2022-11-10 13:58:54 +1300

    add form

--- client/components/MathTest.jsx
+++ client/components/MathTest.jsx
@@ -5,7 +5,22 @@ function MathTest() {
   const [num1, setNum1] = useState(randomNum(10,99))
   const [num2, setNum2] = useState(randomNum(10,99))
   const [sum, setSum] = useState(num1+num2)
+
+  const refreshQuestion = () =>{
+    setNum1(randomNum(10,99))
+    setNum2(randomNum(10,99))
+    setSum(num1+num2)
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    refreshQuestion()
+  }
   
+  // useEffect(()=>{
+  //   console.log('start')
+  // },[])
+
   return (
     <div>
       <div>
@@ -14,6 +29,10 @@ function MathTest() {
           <h2 className=" text-4xl">{num1} + {num2}</h2>
         </div>
         
+        <form onSubmit={handleSubmit}>
+          <input type="text" />
+          <input type="submit" />
+        </form>
       </div>
     </div>
   )
