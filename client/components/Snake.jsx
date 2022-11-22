cc1ce86a4d83ca6e23f36d6559563dd020b85b5b
ashley cao <112398477+ashley-cao-0@users.noreply.github.com>
2022-11-23 11:09:55 +1300

    create matrix with map()

--- client/components/Snake.jsx
+++ client/components/Snake.jsx
@@ -2,16 +2,23 @@
 import React from "react";
 
 function Snake() {
-  const matrix = [[0,0,0], [0,0,0],[0,0,0]]
+
+  const cell = {
+    style: {color: 'blue'}
+  }
+  //create a matrix
+    const grid = Array(8).fill().map(()=>Array(8).fill({...cell}))
+
+
   const handleKeyDown = (e) =>{
     console.log(e.keyCode);
   }
   return (
     <div onKeyDown={handleKeyDown} tabIndex="0" className=" flex flex-col items-center">
-     {matrix.map(row=>
-        <div className="flex">
-          {row.map(cell=>
-            <div className=" w-10 h-10 bg-blue-400 border "></div>
+     {grid.map((row,i)=>
+        <div key={i} className="flex">
+          {row.map((item, i)=>{
+            return <div key={i} className=" w-8 h-8 bg-blue-400 border "></div>}
             )}
         </div>
       )}
