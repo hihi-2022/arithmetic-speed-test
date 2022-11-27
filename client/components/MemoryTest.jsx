6dd9658f1893fd7fb9a3547c4b89ae71f61a990d
Ashley Cao <ashleycao12@gmail.com>
2022-11-27 17:21:53 +1300

    hide digits upon start

--- client/components/MemoryTest.jsx
+++ client/components/MemoryTest.jsx
@@ -3,7 +3,15 @@ import React, {useEffect, useState} from "react";
 const MemoryTest = () => {
   const rowNum = 5
   const colNum = 8
-  const emptyGrid = Array(rowNum).fill().map(()=>Array(colNum).fill(null))
+  const hiddenCell = {backgroundColor: 'white'}
+  const cell ={
+    digit: null,
+    style: {}
+  }
+
+  const gridData = Array(rowNum).fill().map(()=>Array(colNum).fill({...cell}))
+  // const gridDigits = Array(rowNum).fill().map(()=>Array(colNum).fill(null))
+  // const gridStyle = Array(rowNum).fill().map(()=>Array(colNum).fill({}))
 
   const allCoordinates = []
   const coordinateIndexes = []
@@ -17,33 +25,55 @@ const MemoryTest = () => {
 
   const getSeries = () => {
     const newSeries = []
+    const indexes = [...coordinateIndexes]
     for (let i = 0; i < 9; i++) {
-      const randomMetaIndex = randomNum(0, coordinateIndexes.length-1)
-      const randomIndex = coordinateIndexes[randomMetaIndex]
+      const randomMetaIndex = randomNum(0, indexes.length-1)
+      const randomIndex = indexes[randomMetaIndex]
       newSeries.push(allCoordinates[randomIndex])
-      coordinateIndexes.slice(randomMetaIndex, 1)         
+      indexes.slice(randomMetaIndex, 1)         
     }
-    console.log(newSeries);
     return newSeries
   }
 
   const [series, setSeries] = useState(getSeries())
-  const [grid, setGrid] = useState([...emptyGrid])
+  const [grid, setGrid] = useState([...gridData])
+  const [started, setStarted] = useState(false)
 
   const makeGrid = () => {
-    const newGrid = [...emptyGrid]
+    const newGrid = [...gridData]
+    // const newGrid = [...gridDigits]
+    // const newGridStyle = [...gridStyle]
     series.forEach((item, i) => {
       const row = item[0]
       const col = item[1]
-      newGrid[row][col] = i + 1
+      newGrid[row][col] = {...cell}
+      newGrid[row][col].digit = i + 1
+      // gridStyle[row][col] = {...hiddenCell}
+      
     })
+    console.log(newGrid);
     return newGrid
   }
 
+  const hideDigits = () => {
+    series.forEach(item => {
+      const row = item[0]
+      const col = item[1]
+      grid[row][col].style = {...hiddenCell}
+    })
+  }
 
-  // useEffect(async () => {
-  //   setSeries()
-  // })
+  const startGame = () => {
+    console.log('start game');
+    setStarted(true)
+    hideDigits()
+  }
+  
+  const handleClick = () => {
+    if (!started) {
+      startGame()
+    }
+  }
 
   useEffect(() => {
     setGrid(makeGrid())
@@ -56,7 +86,7 @@ const MemoryTest = () => {
         {grid.map((row,i)=>
           <div key={i} className="flex">
             {row.map((item, i)=>{
-              return <div key={i} className=" w-10 h-10 border m-1 text-white text-xl flex items-center justify-center" > <span> {item} </span> </div>}
+              return <div key={i} onClick = {handleClick} className=" w-10 h-10 border m-1 text-white text-xl flex items-center justify-center cursor-pointer" style={item.style} > <span> {item.digit} </span> </div>}
               )}
           </div>)
         }
