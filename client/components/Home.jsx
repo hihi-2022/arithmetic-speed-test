95b2a64bb7c31d83035d61bb44049ce0d68bc9e6
Ashley Cao <ashleycao12@gmail.com>
2022-11-27 14:26:00 +1300

    add snake link to home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,11 +3,12 @@ import {Link} from "react-router-dom"
 
 function Home() {
   return (
-    <div>
+    <div className=" mt-10">
       {/* <h2>Home</h2> */}
       <Link to='/mathtest'>Go to Math</Link> <br></br>
       <Link to='/typingtest'>Go to typing test</Link> <br></br>
-      <Link to='/game'>Go to game</Link>
+      <Link to='/game'>Go to game</Link> <br></br>
+      <Link to='/snake'>Go to snake</Link>
     </div>
   )
 }
